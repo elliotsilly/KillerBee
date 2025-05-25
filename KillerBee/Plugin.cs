39afb1faa2c0a4efc7cf4617087eb86d700595db
Monky <monkygtstudios@gmail.com>
2025-05-25 14:22:55 -0400

    Update Plugin.cs

--- KillerBee/Plugin.cs
+++ KillerBee/Plugin.cs
@@ -6,8 +6,6 @@ namespace KillerBee
     [BepInPlugin("monky.killerbee", "KillerBee", "1.0.0")]
     public class Plugin : BaseUnityPlugin
     {
-        bool inRoom;
-
         void Start()
         {
             Harmony.CreateAndPatchAll(GetType().Assembly, "monky.killerbee");
